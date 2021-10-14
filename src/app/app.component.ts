ed7826cf465b87b3b8b3e6112f83689231173485
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-10-14 17:58:45 -0300

    create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {SocketIoService} from "./socket-io.service";
+import {Message} from "./message";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'chat-app';
+  nickname: string = '';
+  message: string = '';
+  messagesArray: Message[] = [];
+  private subsMessages: any;
+
+
+  constructor(private socketService: SocketIoService) {
+  }
+
+  ngOnInit(){
+   this.subsMessages =  this.socketService.messages().subscribe( (m: Message) => {
+      console.log(m);
+      this.messagesArray.push(m)
+    })
+  }
+
+  send = () => {
+    this.socketService.send({
+      from: this.nickname,
+      message: this.message
+    });
+    this.message = '';
+  }
+
+  ngOnDestroy() {
+    this.subsMessages.unsubscribe();
+  }
 }
---
+++ src/app/socket-io.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import * as socketIo from 'socket.io-client'
+import {Message} from "./message";
+import {Subject} from "rxjs";
+import {io} from "socket.io-client";
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketIoService {
+
+  // TODO change this later
+  url = 'http://localhost:4444';
+  socket = io(this.url);
+  subjectMessages: Subject<Message> = new Subject<Message>();
+
+  constructor() {
+    this.socket.on('message', (m: Message) => {
+      this.subjectMessages.next(m);
+    });
+  }
+
+   send = (msg: Message) => {
+    this.socket.emit('message', msg)
+   }
+
+   messages = () => {
+    return this.subjectMessages.asObservable();
+   }
+}
